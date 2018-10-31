e1cf705ab65018c5512f3f8f78e0d52f0fa4e259
noenadies <33671023+noenadies@users.noreply.github.com>
2018-10-31 13:32:05 -0500

    Add files via upload

--- sit.js
+++ sit.js
@@ -71,7 +71,7 @@ document.getElementById("idpinforme").innerHTML ="Informe:"+"<br>"+
 function imprimir(){
 window.print();
 
-if(w<1100){
+if(w<1500){
   window.Android.showToast("pdf");
 }
 }
@@ -156,7 +156,7 @@ for(var i in general){
   if(general[i].keymi==lemail&&general[i].pass==lpass){
     ahorasi(nodeusersit,general[i].keymi,"buscar");
     nodousuario=general[i].keymi;
-    if(w<1100){
+    if(w<1500){
 window.Android.showToast("escribetxt",String(nodousuario));}
     alert("buscando login");
      setTimeout(login, timeespera);
@@ -172,7 +172,7 @@ window.Android.showToast("escribetxt",String(nodousuario));}
 
 
 
- if(w<1100){
+ if(w<1500){
 
     if(bollactivar){logeado();
      alert("buscando desde ya");}
@@ -251,7 +251,7 @@ document.getElementById("idbtrefres").style.display = "flex";
  document.getElementById("idbtrefres").style.top =String(h/2+100)+ "px";
      document.getElementById("idpinforme").style.top =String(h/2+400)+ "px";
 
-     if(w<1100){
+     if(w<1500){
 
     document.getElementById("idpinforme").style.top =String(h/2-400)+ "px";
      }
@@ -634,8 +634,8 @@ var resdeandroid="no";
 setTimeout(esperar,3000);
 function esperar(){
 
-  document.getElementById("idh1").innerHTML="Espere profavor .... ";
-if(w<1100){
+  document.getElementById("idh1").innerHTML="Espere porfavor .... ";
+if(w<1500){
 resdeandroid= window.Android.showToast("nodo");
 resdeandroid =String(resdeandroid);
 document.getElementById("idh1").innerHTML=resdeandroid;
