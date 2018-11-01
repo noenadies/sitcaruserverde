81d437ce9ee64d40df8539ba448693cfbb746e1b
noenadies <33671023+noenadies@users.noreply.github.com>
2018-11-01 16:32:00 -0500

    Add files via upload

--- sit.js
+++ sit.js
@@ -289,7 +289,7 @@ if(w<600)
 {
 document.getElementById("idpinforme").style.display = "none";
 
-document.getElementById("idp").style.fontSize = "15px";
+document.getElementById("idp").style.fontSize = "23px";
 
 }
 setTimeout(llamamapa,1000);
@@ -713,8 +713,11 @@ function denuevo2(){
  my=0;//4.6474131;//mx
 mx=0;
 function esmapa3d(){
+  //  document.getElementById("idt").style.display = "flex";
 
+document.getElementById("idp").style.display = "flex";
 
+document.getElementById("idp").style.fontSize = "23px";
      my=varcarlat;//4.6474131;//mx
      mx=varcarlng;//-74.1543345; //my
 console.log(my+"  l-nf   "+mx);
